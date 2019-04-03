15483f2ec636aee7be0ef3b024800a012625418a
GentBinaku <binakugent@gmail.com>
2019-04-03 16:08:38 +0200

    This is about delivering templates

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + "/../public"));
 const bodyParser = require('body-parser');
 
 
 const PORT = 3000;
+
+
+app.get('/', function(req, res){
+  res.render('index.ejs');
+});
 
 app.listen(PORT, function(){
    console.log('Server is running on Port',PORT);
