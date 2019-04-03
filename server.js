c60e07ca6458ca6cda2b91487954fe27d19f8813
GentBinaku <binakugent@gmail.com>
2019-04-03 16:12:43 +0200

    s

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const bodyParser = require('body-parser');
 const PORT = 3000;
 
 
-app.get('/', function(req, res){
-  res.render('index.js');
-});
-
 app.listen(PORT, function(){
    console.log('Server is running on Port',PORT);
 });
