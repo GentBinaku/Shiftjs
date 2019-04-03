2cf40132f6f72e8053a2e0c58a73b1f41552502c
GentBinaku <binakugent@gmail.com>
2019-04-03 16:14:59 +0200

    checking

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
+app.get('/checking', function(req, res){
+  res.json({
+     "Tutorial": "Welcome to the Node express JWT Tutorial"
+  });
+});
 
 app.listen(PORT, function(){
    console.log('Server is running on Port',PORT);
