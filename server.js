fd6a0146aca5847730df2b45bb1a3222abb0ca12
GentBinaku <binakugent@gmail.com>
2019-04-05 16:09:34 +0200

    tried doint to MongoLab

--- server.js
+++ server.js
@@ -1,10 +1,59 @@
-var MongoClient = require('mongodb').MongoClient;
+'use strict';
 
-// Connect to the db
-MongoClient.connect("mongodb://mongodb-6-servers-vm-0/local?replicaSet=rs0", function (err, db) {
-   
-     if(err) throw err;
+const mongodb = require('mongodb');
+const http = require('http');
+const nconf = require('nconf');
+let uri = ` PASTE YOUR MONGODB ATLAS CONNECTION STRING HERE `;
+if (nconf.get('mongoDatabase')) {
+  uri = `${uri}/${nconf.get('mongoDatabase')}`;
+}
+console.log(uri);
 
-     //Write databse Insert/Update/Query code here..
-                
+mongodb.MongoClient.connect(uri, (err, db) => {
+  if (err) {
+    throw err;
+  }
+
+  // Create a simple little server.
+  http.createServer((req, res) => {
+    if (req.url === '/_ah/health') {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('OK');
+      res.end();
+      return;
+    }
+
+    const collection = db.collection('Messages');
+    var datetime = new Date();
+    const msg = {
+      msgDescription: '\nHello World received on ' + datetime
+    };
+
+    collection.insert(msg, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      // push out a range
+      let msglist = '';
+      collection.find().toArray((err, data) => {
+        if (err) {
+          throw err;
+        }
+        data.forEach((msg) => {
+          msglist += `${msg.msgDescription}; `;
+        });
+
+        res.writeHead(200, {
+          'Content-Type': 'text/plain'
+        });
+res.write('Messages received so far:\n');
+        res.end(msglist);
+      });
+    });
+  }).listen(process.env.PORT || 8080, () => {
+    console.log('started web process');
+  });
 });
