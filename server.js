06bd93d564c6d1f067fc7ce6d9d156fd261bd538
GentBinaku <binakugent@gmail.com>
2019-04-05 14:40:28 +0200

    Trying mongod

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
+var MongoClient = require('mongodb').MongoClient;
 
+// Connect to the db
+MongoClient.connect("'mongodb://mongodb-6-servers-vm-0/users?replicaSet=rs0'", function (err, db) {
+   
+     if(err) throw err;
 
-//connect to MongoDB
-mongoose.connect('mongodb://mongodb-6-servers-vm-1');
-var db = mongoose.connection;
-
-//handle mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
+     //Write databse Insert/Update/Query code here..
+                
 });
