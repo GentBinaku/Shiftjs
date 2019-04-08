2c8999afa88e8bd53e60d93ee14a7e31be869a92
GentBinaku <binakugent@gmail.com>
2019-04-08 14:42:39 +0200

    Trying to complete login first

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-var mongoose = require('mongoose')
-var session = require('express-session')
-var MongoStore = require('connect-mongo')(session)
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-mongoose.connect('mongodb://root:root@35.195.208.33:27017,35.187.114.130:27017/users?replicaSet=rs0')
+//connect to MongoDB
+mongoose.connect('mongodb://root:root@mongodb-6-servers-vm-1:27017,mongodb-6-servers-vm-0:27017/admin?replicaSet=rs0',{ useNewUrlParser: true });
 var db = mongoose.connection;
 
-//handle mongo error
+//handle mongcd o error
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  // we're connected!
+ process.stdout.write("hello: ");
+})
+
+app.use(session({
+      secret:'work hard',
+      resave : true,
+      saveUninitialized: false,
+      store:new MongoStore({
+        mongooseConnection : db
+      })
+}))
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// serve static files from template
+app.use(express.static(__dirname + '/templateLogReg'));
+
+//include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('Express app listening on port 3000');
 });
